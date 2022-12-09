7b75943f7f44c2c76dd6acc5cce5e716e464fe9b
Alex Gilmer <alex.gilmer@mitt.ca>
2022-12-09 09:22:47 -0600

    Add project files.

---
+++ Directory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_07
+{
+    internal class Directory
+    {
+        public Directory? Parent { get; set; }
+        public HashSet<Directory> Children { get; } = new();
+        public string Name { get; set; }
+        public Dictionary<string, int> Files { get; } = new();
+        public int TotalFileSize
+        {
+            get
+            {
+                return Files.Sum(f => f.Value);
+            }
+        }
+        public int TotalChildSize
+        {
+            get
+            {
+                return Children.Sum(c => c.GetSize());
+            }
+        }
+        public Directory(string name)
+        {
+            Name = name;
+        }
+
+        public int GetSize()
+        {
+            int result = 0;
+            foreach ((string name, int size) in Files)
+            {
+                result += size;
+            }
+            foreach (Directory directory in Children)
+            {
+                result += directory.GetSize();
+            }
+            return result;
+        }
+
+        public void PrintContents()
+        {
+            Console.WriteLine($"Printing directory.  Name: {Name}, File count: {Files.Count}, Child count: {Children.Count}");
+            foreach ((string n, int s) in Files)
+            {
+                Console.WriteLine($"\t{n,-10}\t{s}");
+            }
+            foreach(Directory d in Children)
+            {
+                Console.WriteLine($"\tChild: {d.Name,-10}\t{d.GetSize()}");
+            }
+        }
+
+        public void PrintContentsCompact()
+        {
+            Console.WriteLine($"Directory: {Name}, {Files.Count} Files worth {TotalFileSize} bytes.  {Children.Count} Children worth {TotalChildSize} bytes.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Security;
+
+namespace Advent_Of_Code_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory home = new("/");
+            Directory currentDirectory = home;
+            HashSet<Directory> Directories = new()
+            {
+                home
+            };
+
+            bool collectingData = true;
+
+            // first input puts us in home directory
+            string input = GetInput("Terminal string:");
+
+            while (collectingData)
+            {
+                input = GetInput("Terminal string:");
+
+                if (input == "")
+                {
+                    break;
+                }
+
+                string[] parsedInput = input.Split(' ');
+
+                if (parsedInput[0] == "$" && parsedInput[1] == "cd")
+                {
+                    if (parsedInput[2] == "..")
+                    {
+                        if (currentDirectory.Parent != null)
+                        {
+                            currentDirectory = currentDirectory.Parent;
+                        }
+                    }
+                    else
+                    {
+                        var newDirectory = new Directory(parsedInput[2]);
+                        Directories.Add(newDirectory);
+
+                        if (currentDirectory != null)
+                        {
+                            currentDirectory.Children.Add(newDirectory);
+                        }
+                        newDirectory.Parent = currentDirectory;
+                        currentDirectory = newDirectory;
+                    }
+                }
+
+                if (int.TryParse(parsedInput[0], out int num))
+                {
+                    currentDirectory!.Files[parsedInput[1]] = num;
+                }
+            }
+
+            Console.Clear();
+            int result = 0;
+            const int threshold = 100000;
+            var finalListing = Directories.OrderBy(d => d.Name);
+            foreach (Directory d in finalListing)
+            {
+                if (d.GetSize() <= threshold)
+                {
+                    result += d.GetSize();
+                }
+                d.PrintContentsCompact();
+            }
+            Console.WriteLine($"total of items <= {threshold}: {result}");
+        }
+
+        static string GetInput(string prompt)
+        {
+            bool validResult = false;
+            string? input = null;
+            while (!validResult)
+            {
+                Console.WriteLine(prompt);
+                input = Console.ReadLine();
+                if (input != null)
+                {
+                    validResult = true;
+                }
+            }
+
+            return input!;
+        }
+    }
+}
